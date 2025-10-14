b7fdcdc81bff115da70a56064e190414e8062aec
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2025-10-14 18:56:10 -0300

    Upload dos códigos da lista de exercícios

---
+++ exercicio4_tempo_por_thread.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <omp.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+    const int N = 1000000;
+    double *x = new double[N];
+    double *y = new double[N];
+    double *z = new double[N];
+    double *a = new double[N];
+
+    for (int i = 0; i < N; i++) {
+        x[i] = i * 0.001;
+        y[i] = i * 0.002;
+        z[i] = i * 0.003;
+    }
+
+    double inicio_total = omp_get_wtime();
+
+    #pragma omp parallel
+    {
+        double inicio = omp_get_wtime();
+        #pragma omp for
+        for (int i = 0; i < N; i++) {
+            a[i] = pow(x[i], 2) + pow(y[i], 2) + pow(z[i], 2);
+        }
+        double fim = omp_get_wtime();
+        int id = omp_get_thread_num();
+        cout << "Thread " << id << " executou em " << fim - inicio << " segundos." << endl;
+    }
+
+    double fim_total = omp_get_wtime();
+    cout << "Tempo total de execução: " << fim_total - inicio_total << " segundos." << endl;
+
+    delete[] x;
+    delete[] y;
+    delete[] z;
+    delete[] a;
+
+    return 0;
+}
