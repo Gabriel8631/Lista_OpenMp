9d2a2cb5a7da383855349b386ee51ad32781ea51
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2025-10-14 19:01:16 -0300

    Update exercicio1_hello_world.cpp

--- exercicio1_hello_world.cpp
+++ exercicio1_hello_world.cpp
@@ -1,3 +1,8 @@
+/*Exercício 1 — “Hello World” Paralelo
+a) Crie uma região paralela com #pragma omp parallel.
+b) Cada thread deve imprimir uma mensagem indicando seu número (omp_get_thread_num()) e o total de threads (omp_get_num_threads()).
+c) Configure o programa para rodar com 4 threads.*/
+
 #include <iostream>
 #include <omp.h>
 using namespace std;
