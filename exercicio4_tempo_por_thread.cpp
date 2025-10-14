63d6353db1ab97e4d7e22ac37f6f4b78d06b028f
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2025-10-14 19:03:12 -0300

    Update exercicio4_tempo_por_thread.cpp

--- exercicio4_tempo_por_thread.cpp
+++ exercicio4_tempo_por_thread.cpp
@@ -1,3 +1,9 @@
+/*Exercício 4 — Medindo tempo por thread
+Adapte o código do exercício anterior para:
+a) Medir e exibir o tempo total de execução.
+b) Medir e exibir o tempo gasto por cada thread.
+c) Mostrar quantas threads foram utilizadas no cálculo.*/
+
 #include <iostream>
 #include <omp.h>
 #include <cmath>
