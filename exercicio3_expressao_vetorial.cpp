35d1c689b01cc99d004fa0e4bd08bfc804dee242
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2025-10-14 19:02:47 -0300

    Update exercicio3_expressao_vetorial.cpp

--- exercicio3_expressao_vetorial.cpp
+++ exercicio3_expressao_vetorial.cpp
@@ -1,3 +1,9 @@
+/*Exercício 3 — Expressão Vetorial
+Implemente a expressão: a[i] = x[i]^2 + y[i]^2 + z[i]^2 para vetores de tamanho 1.000.000.
+a) Faça a versão sequencial.
+b) Paralelize com #pragma omp parallel for schedule(static).
+c) Mostre o tempo total de execução em cada versão.*/
+
 #include <iostream>
 #include <omp.h>
 #include <cmath>
