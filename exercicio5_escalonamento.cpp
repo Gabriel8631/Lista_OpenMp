7d43583db2723f2b4c8656b53c954fc7c56fe873
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2025-10-14 19:03:41 -0300

    Update exercicio5_escalonamento.cpp

--- exercicio5_escalonamento.cpp
+++ exercicio5_escalonamento.cpp
@@ -1,3 +1,9 @@
+/*Exercício 5 — Escalonamento
+Use novamente o cálculo de a[i] = x[i]^2 + y[i]^2 + z[i]^2, mas:
+a) Execute com schedule(static) e schedule(dynamic, 1000).
+b) Compare os tempos em diferentes quantidades de threads (2, 4, 8).
+c) Explique em quais situações static e dynamic são mais adequados.*/
+
 #include <iostream>
 #include <omp.h>
 #include <cmath>
